94ca5d5641ad7afa363ed2ed318e5fbbf01f02f3
Dan Hedgecock <daniel.hedgecock@healthsparq.com>
2015-06-30 13:37:01 -0700

    Updating user reference in chat sidebar

--- js/app/components/chat-sidebar.js
+++ js/app/components/chat-sidebar.js
@@ -1,15 +1,42 @@
 App.ChatSidebarComponent =  Ember.Component.extend({
+  /**
+   * Boolean for whether chat sidebar is active
+   *
+   * @type {boolean}
+   */
   isActive: false,
-  currentInput: '',
-  store: Ember.computed(function() {
-    return this.get('container').lookup('store:main');
-  }),
 
+  /**
+   * Current value for chat sidebar input
+   *
+   * @type {string}
+   */
+  currentInput: '',
+
+  /**
+   * Current authentication status
+   *
+   * @type {Object}
+   */
+  auth: Ember.inject.service('auth'),
+
+  /**
+   * Ensure most recent message is displayed when element is inserted
+   *
+   * @memberof App.ChatSidebar
+   * @instance
+   */
   didInsertElement: function() {
     // Ensure messages default to most recent
     $('.js-chat-messages').scrollTop(999);
   },
 
+  /**
+   * Scroll to most recent message in chat box
+   *
+   * @memberof App.ChatSidebarComponent
+   * @instance
+   */
   scrollMessages: function() {
     var messages = $('.js-chat-messages');
     var height = messages.height();
@@ -24,7 +51,10 @@ App.ChatSidebarComponent =  Ember.Component.extend({
     postMessage: function() {
       var store = this.get('store');
       var message = this.get('currentInput');
+      var user = this.get('auth.user');
       var _this = this;
+
+      console.log('current user', user);
       
       store.find('user', 1).then(function(resp) {
         // Send This to FireBase
