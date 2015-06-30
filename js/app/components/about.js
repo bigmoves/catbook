51a4ebdc205f40f9b031e59a28e6c39e95831881
Matthew Salcido <matthew.salcido@healthsparq.com>
2015-06-30 16:47:36 -0700

    user editable hometown,relationship fields

--- js/app/components/about.js
+++ js/app/components/about.js
@@ -1,7 +1,32 @@
 App.AboutProfileComponent = Ember.Component.extend({
+
+  showHometown: false,
+  showRelationship: false,
+
+  user: Ember.computed.alias('model'),
+
   actions: {
-    toggleConsole: function() {
-      console.log("click");
+    editHome: function() {
+      this.toggleProperty('showHometown');
+    },
+    submitHometown: function(hometown) {
+      var component = this;
+      var user = this.get('user');
+      user.set('hometown', hometown);
+      user.save().then(function() {
+        component.set('showHometown', false);
+      });
+    },
+    editRelationship: function(){
+      this.toggleProperty('showRelationship');
+    },
+    submitRelationship: function(relationship) {
+      var component = this;
+      var user = this.get('user');
+      user.set('relationshipStatus', relationship);
+      user.save().then(function() {
+        component.set('showRelationship', false);
+      });
     }
   }
 });
